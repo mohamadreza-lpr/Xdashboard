e4b95382ac20cc783aede3509c572621100ad276
m.laalpour <m.laalpour@mobileabad.com>
2022-09-11 16:18:41 +0430

    added global components

--- src/main.js
+++ src/main.js
@@ -8,13 +8,14 @@ import VueApexCharts from 'vue-apexcharts'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import Xcard from '@/components/index'
+import XGlobalComponents from '@/components/index'
 
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.use(VueApexCharts)
+Vue.use(XGlobalComponents)
 Vue.component('apexchart', VueApexCharts)
 new Vue({
   router,
