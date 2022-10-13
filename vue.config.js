a77e49974677487cc5c1c24adb13fef10f7fc22d
m.laalpour <m.laalpour@mobileabad.com>
2022-10-13 12:06:06 +0330

    added sass loader to the project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false
-})
+    transpileDependencies: true,
+    lintOnSave: false,
+    pluginOptions: {
+        "style-resources-loader": {
+            preProcessor: "scss",
+            // load which style file you want to import globally
+            patterns: [path.resolve(__dirname, "./src/styles/_variables.scss")],
+        },
+
+    },
+    // css: {
+    //     loaderOptions: {
+    //         sass: {
+    //             data: `@import "@/styles/_variables.scss";`,
+    //         },
+    //     },
+    // },
+});
